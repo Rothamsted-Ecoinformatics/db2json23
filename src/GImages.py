c59cd94d0e5387ac7515d8d831e3783bdc4f95d9
Nathalie Castells-Brooke <nathalie.castells@rothamsted.ac.uk>
2024-07-16 18:14:51 +0100

    add content URL and update-all

--- src/GImages.py
+++ src/GImages.py
@@ -149,7 +149,6 @@ if __name__ == '__main__':
     stLoc = getStored()
     for loc in stLoc: 
         print(loc)
-        
     
     play = input ("---- {} images in the BASSET drive --------------".format(len(stLoc)))
 
